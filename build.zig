24da50a15a34b4147e11fa41fcc67b5d0d12932f
Will M <wdmingu@gmail.com>
2025-02-16 10:29:41 -0700

    Working triangle example

--- build.zig
+++ build.zig
@@ -113,4 +113,7 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    exe.linkSystemLibrary("raylib");
+    exe.linkLibC();
 }
--- src/main.zig
+++ src/main.zig
@@ -1,45 +1,30 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+const ray = @cImport({
+    @cInclude("raylib.h");
+});
+
+const title = "Triangle!";
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const windows_width = 600;
+    const windows_height = 400;
+    const color = ray.ORANGE;
+    const bgcolor = ray.BLACK;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    ray.InitWindow(windows_width, windows_height, title);
+    defer ray.CloseWindow();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    ray.SetTargetFPS(60);
 
-    try bw.flush(); // Don't forget to flush!
+    while (!ray.WindowShouldClose()) {
+        ray.BeginDrawing();
+        defer ray.EndDrawing();
+
+        ray.ClearBackground(bgcolor);
+
+        const a = ray.Vector2{ .x = 50.0, .y = 200.0 };
+        const b = ray.Vector2{ .x = 200.0, .y = 50.0 };
+        const c = ray.Vector2{ .x = 50.0, .y = 50.0 };
+        ray.DrawTriangle(a, b, c, color);
+    }
 }
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(global.testOne, .{});
-}
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("zig-pong_lib");
